e5cb11e8c42e0c651e45bea7166b1065156dc4f1
Berat Hündürel <berat.hundurel@bilgiedu.net>
2024-07-18 22:53:20 +0300

    Mobile animations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,12 +12,12 @@
    let image4: HTMLImageElement;
    let imageMain: HTMLImageElement;
 
-   const animateImage = (img: HTMLImageElement, x: number, y: number, isLast: boolean) => {
+   const animateImage = (img: HTMLImageElement, x: number, y: number, isLast: boolean, end?: string, mainX?: number, mainY?: number) => {
       gsap.to(img, {
          scrollTrigger: {
             trigger: img,
             start: "+=75",
-            end: "+=4250",
+            end: end || "+=4250",
             scrub: 1,
          },
          x,
@@ -27,8 +27,8 @@
                gsap.to(imageMain, {
                   rotate: 30,
                   duration: 2,
-                  y: -50,
-                  x: -300,
+                  y: mainY || -50,
+                  x: mainX || -300,
                   ease: "bounce.inOut",
                });
 
@@ -36,6 +36,7 @@
                   gsap.to(img, {
                      opacity: 0,
                      duration: 0,
+                     ease: "back.in",
                   });
                });
             }
@@ -79,10 +80,21 @@
          });
       }
 
-      animateImage(image, 450, -5, false);
-      animateImage(image2, 250, 85, false);
-      animateImage(image3, -225, -45, false);
-      animateImage(image4, -250, 225, true);
+      ScrollTrigger.matchMedia({
+         "(min-width: 768px)": () => {
+            console.log("desktop");
+            animateImage(image, 450, -5, false);
+            animateImage(image2, 250, 85, false);
+            animateImage(image3, -225, -45, false);
+            animateImage(image4, -250, 225, true);
+         },
+         "(max-width: 767px)": () => {
+            animateImage(image, 150, -150, false);
+            animateImage(image2, 100, 175, false);
+            animateImage(image3, -125, -100, false);
+            animateImage(image4, -80, 300, true, "+=2000", 20, -50);
+         },
+      });
 
       gsap.to("progress", {
          value: 100,
@@ -95,25 +107,28 @@
    });
 </script>
 
-<div class="relative max-w-[88rem] mx-auto" id="page">
-   <div class="flex items-center w-full h-[95svh] justify-start overflow-hidden whitespace-nowrap flex-nowrap list-none" bind:this={textContainer}>
-      <p class="text-slate-100 text-8xl text inline-block font-bold tracking-tighter">
+<div class="relative max-w-[88rem] px-3 lg:px-0 mx-auto" id="page">
+   <div
+      class="flex items-center w-full lg:h-[95svh] h-[90svh] justify-start overflow-hidden whitespace-nowrap flex-nowrap list-none"
+      bind:this={textContainer}
+   >
+      <p class="text-slate-100 lg:text-8xl text-5xl text inline-block font-bold tracking-tighter">
          Explore Art Around the world, Beautiful Artifacts, Memory to Remember Learn, Know and See History..
       </p>
-      <div class="absolute bottom-10 right-[55%] -z-10">
-         <img src="/image1.png" alt="art-img" class="w-[300px] rounded-sm" bind:this={image} />
+      <div class="absolute lg:bottom-10 bottom-0 right-[55%] -z-10">
+         <img src="/image1.png" alt="art-img" class="lg:w-[300px] w-60 rounded-sm" bind:this={image} />
       </div>
-      <div class="absolute bottom-36 right-[42.5%] -z-20">
-         <img src="/image4.png" alt="art-img" class="w-[300px] h-[400px] rounded-md" bind:this={image2} />
+      <div class="absolute lg:bottom-36 bottom-52 right-[42.5%] -z-20">
+         <img src="/image4.png" alt="art-img" class="lg:w-[300px] lg:h-[400px] w-52 h-64 rounded-md" bind:this={image2} />
       </div>
-      <div class="absolute bottom-10 right-[22.5%] -z-10">
-         <img src="/image2.png" alt="art-img" class="w-[350px] h-[450px] rounded-md" bind:this={imageMain} />
+      <div class="absolute lg:bottom-10 bottom-14 right-[22.5%] -z-10">
+         <img src="/image2.png" alt="art-img" class="lg:w-[350px] lg:h-[450px] w-56 h-72 rounded-md" bind:this={imageMain} />
       </div>
-      <div class="absolute bottom-0 right-[7.5%] -z-20">
-         <img src="/image3.png" alt="art-img" class="w-[250px] h-[300px] rounded-md" bind:this={image3} />
+      <div class="absolute bottom-0 lg:right-[7.5%] right-[5%] -z-20">
+         <img src="/image3.png" alt="art-img" class="lg:w-[250px] lg:h-[300px] w-44 h-52 rounded-md" bind:this={image3} />
       </div>
       <div class="absolute top-1 right-[7.5%] -z-20">
-         <img src="/image5.png" alt="art-img" class="w-[300px] h-[300px] rounded-md" bind:this={image4} />
+         <img src="/image5.png" alt="art-img" class="lg:w-[300px] lg:h-[300px] w-52 h-52 rounded-md" bind:this={image4} />
       </div>
       <progress class="fixed bottom-2 right-0 z-50 appearance-none h-1 w-full bg-transparent" max="100" value="0"></progress>
       <progress class="fixed top-6 left-0 z-50 appearance-none h-1 w-full bg-transparent rotate-180" max="100" value="0"></progress>
